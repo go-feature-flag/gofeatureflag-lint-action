97c05d89ec53bda26f52e5c0990e3d14f254b438
Sarvsav Sharma <sarvsav@gmail.com>
2023-04-29 06:55:07 +0200

    Added debug command

--- run-lint.sh
+++ run-lint.sh
@@ -1,5 +1,7 @@
 ## Get the latest image of go-feature-flag-lint
 docker pull thomaspoignant/go-feature-flag-lint:latest
 
+echo "Current working directory is $1"
+
 ## Run the linter against the config file
-docker run -v ${pwd}/config:/config --rm --name gofeatureflag_lint thomaspoignant/go-feature-flag-lint --input-format=yaml --input-file=/config/flag-config.yaml
+docker run -v ${1}/config:/config --rm --name gofeatureflag_lint thomaspoignant/go-feature-flag-lint --input-format=yaml --input-file=/config/flag-config.yaml
